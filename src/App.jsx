fa47438cdc0bf0370ab101289d57a960ba8e7f1f
Satyajit Samal <samalsatyajit668@gmail.com>
2024-08-12 20:14:25 +0530

    Added Login, Profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import UserContextProvider from './context/UserContextProvider';
+import Login from './components/Login';
+import Profile from './components/Profile';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <h1>Context API</h1>
-    </>
+  return(
+    <UserContextProvider>
+    <h2>Context API</h2>
+    <Login />
+    <Profile />
+    </UserContextProvider>
   )
 }
 
-export default App
+export default App;
+
+
